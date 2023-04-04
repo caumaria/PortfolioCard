1df42c39503545d55cab026d0f4b0242c7c41670
caumaria <cau.ugolini@gmail.com>
2023-04-04 16:52:40 -0300

    Homepage added

--- PortfolioCard/src/components/homepage/Homepage.jsx
+++ PortfolioCard/src/components/homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
 import styled from 'styled-components';
 
 const Background = styled.div`
@@ -8,21 +8,107 @@ const Background = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  font-family: 'Inconsolata', monospace;
 `;
 
 const Container = styled.div`
   height: 90vh;
-  width: 96vw;
-  border: 2px solid white;
+  width: 90vw;
+  border: 1px solid #c3c3c3;
   background-color: hsl(0, 0%, 5%);
+  position: fixed;
 `;
 
+const Header = styled.div`
+  width: 16rem;
+  height: 20rem;
+  color: #c3c3c3;
+  margin: 2rem;
+`;
+
+const H1 = styled.h1`
+  font-weight: 200;
+  font-size: 40px;
+  padding-bottom: .5rem;
+`;
+
+const Flex = styled.div`
+  display: flex,
+`;
+
+const Ul = styled.ul`
+  list-style: none;
+  cursor: pointer;
+  padding-top: 2rem;  
+`;
+
+const Li = styled.li`
+  padding-top: .8rem;
+  font-weight: 900;
+`;
+
+const About = styled.div`
+  width: 12rem;
+  height: 16rem;
+  color: #c3c3c3;
+  margin: 2rem;
+  position: absolute;
+  bottom: 0;
+  right: 0;  
+`;
 
 const Homepage = () => {
+  const webD = useRef(null);
+
+  useEffect(() => {
+    const typedWeb = new Typed(webD.current, {
+      strings: ['.web developer'],
+      startDelay: 400,
+      typeSpeed: 70,
+      backDelay: 500,           
+    });
+
+    return () => {
+      typedWeb.destroy();
+    };
+  }, []);
+
   return (
     <Background>
       <Container>
-        this is the HOME PAGE
+        <Header>
+          <H1>Cau Ugolini</H1>
+          <Flex>
+            <pre ref={webD}></pre>
+          </Flex>         
+
+          <Ul>
+            <Li>Home</Li>
+            <Li>Projects</Li>
+            <Li>Contact</Li>
+          </Ul>
+
+        </Header>
+
+        <About>
+          <p>
+            Born in 1815.
+          </p>
+          <p>
+            Caindo aos pedaços, lutando pela  
+            sobrevivencia nesse mundo cruel.
+            asha ara ara lal ala alalala
+            ashalalala lenga lenga WE NEED MORE WORDS, WATSON.
+            <br></br>
+            <br></br>
+            Resume about cacadinha's life.
+            <br></br>
+            <br></br>
+            Coming soon.
+          </p>
+        </About>
+
+
       </Container>      
     </Background>
   )
--- PortfolioCard/src/components/splash screen/Splash.jsx
+++ PortfolioCard/src/components/splash screen/Splash.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styled from 'styled-components';
-
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   height: 100vh;
@@ -15,17 +15,33 @@ const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: white;
+  display: flex;
 `;
 
-
 const Splash = () => {
+  const name = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(name.current, {
+      strings: ["cau. "],
+      startDelay: 600,
+      typeSpeed: 180,      
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+
   return (
     <Container>
       <Title>
-        cau. portfolio
+        <pre ref={name}></pre>
+        <pre id="portfolio">portfolio</pre>
       </Title>
     </Container>
-  )
+  );
 };
 
 export default Splash;
