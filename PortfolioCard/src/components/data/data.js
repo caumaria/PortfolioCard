69fc0589d93cce2b89b79435c1f1f959c70165f2
Cau <cau.ugolini@gmail.com>
2023-07-18 09:37:20 -0300

    docs: url projects

--- PortfolioCard/src/components/data/data.js
+++ PortfolioCard/src/components/data/data.js
@@ -3,27 +3,27 @@ export const projects = [
       name: "sneakers",
       projects: "Landing page for products",
       year: "frontendmentor challenge, 2023.",
-      url: 'https://github.com/caumaria/product',
-      web: 'https://product-cau.vercel.app'
+      url: 'https://github.com/caumaria/productLandingPage',
+      web: 'https://productlandingpage-cau.vercel.app'
     },
     {
       name: "Crypto Kingdom",
       projects: "Crypto Tracker App • Coingecko API",
-      year: "Personal, 2023.",
+      year: "Study case, 2023.",
       url: 'https://github.com/caumaria/CryptoKingdom',
       web: 'https://crypto-kingdom.vercel.app'
     },
     {
       name: "Diary App",
       projects: "CRUD app • TodoList & Pomodoro Timer",
-      year: "Study case, 2023.",
+      year: "Personal, 2023.",
       url: 'https://github.com/caumaria/diary',
-      web: 'https://productlandingpage-cau.vercel.app'
+      web: 'https://diary-cau.vercel.app'
     },
     {
       name: "Web & Mobile App",
       projects: "19 projects",
-      year: "Study case, 2023.",
+      year: "2022 • 2023.",
       url: 'https://github.com/caumaria',
       web: 'https://github.com/caumaria'
     },
